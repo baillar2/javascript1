32b18d713673c8d63166051c8ad6f79ef1420b17
Student <student@RU-002.local>
2016-01-18 12:03:55 -0700

    finished string

--- mainTwo.js
+++ mainTwo.js
@@ -2,3 +2,12 @@
 
  console.log(anyWord.length)
  console.log(anyWord)
+ console.log(anyWord.charAt(2))
+ console.log(anyWord.toLowerCase())
+ console.log(anyWord.toUpperCase())
+ console.log("Here is your word in a " + anyWord + " sentance!")
+ 
+ for (var i=1; i<=3; i++) {
+	console.log(anyWord.charAt(i))
+}
+console.log(anyWord.substr(1,3))
