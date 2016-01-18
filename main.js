1de062faa5191f0e25ed303b8658e7a2531c84a5
Student <student@RU-002.local>
2016-01-18 11:44:59 -0700

    finished loop 1

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-alert('hello boulder!');
+//alert('hello boulder!');
+
+
+//var userName = prompt ("what is your name", "")
+
+//console.log ("Hello " + userName + "!")
+
+for (var i = 0; i < 600; i+=100) {
+	console.log(i)
+}
+
+for (var i = 1; i < 65; i *= 2) {
+	console.log(i)
+}
+
+for (var i = 1; i <= 3; i++) {
+	for(var j = 1; j <=3; j++) {
+		console.log(i)
+	}
+}
+
+for (var i=0; i <=10; i+=2){
+	console.log(i)
+}
+
+for (var i=3; i<=15; i+=3){
+	console.log(i)
+}
+
+for (var i=9; i>=0; i--){
+	console.log(i)
+}
+
+for (var i = 0; i <= 4; i++) {
+	for(var j = 0; j <=3; j++) {
+		console.log(j)
+	}
+}
